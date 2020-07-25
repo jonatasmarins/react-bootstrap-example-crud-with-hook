f86540f1da7d7086c25d13209f898cc8d5437a0e
jonatasmarins <jonatasmarins_leite@hotmail.com>
2020-07-25 19:54:19 -0300

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -53,7 +53,6 @@ export default function App() {
   }
 
   const onRefresh = async () => {
-    console.log(periodSelected)
     await onChangeDateSelect(periodSelected);
     await getTransactionByPeriod(periodSelected);
   } 
